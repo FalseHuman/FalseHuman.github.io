4859f14445e03af7d43c228706fdb1d977bedbad
Emil <emilkhazioff@yandex.com>
2020-08-13 21:15:42 +0500

    Хз какая попытка

---
+++ script.js
@@ -0,0 +1,60 @@
+let phrases = [
+  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+{ text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+{ text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
+{ text: 'посмотреть видосик на YouTube', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
+{ text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+{ text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
+{ text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
+{ text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
+{ text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
+{ text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
+];
+
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
+
+let button = document.querySelector('.button');
+let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
+let YouTube = document.querySelector('.YouTube');
+let WhiteMode = document.querySelector('.WhiteMode');
+let Yandex = document.querySelector('.Yandex');
+
+button.addEventListener('click', function () {
+  let randomElement = getRandomElement(phrases);
+  image.src = randomElement.image
+  phrase.textContent = randomElement.text;
+
+  if (randomElement.text.length > 40) {
+    advice.style.fontSize = '33px';
+  } else {
+    advice.style.fontSize = '42px';
+  }
+});
+button.addEventListener('click', function () {
+  smoothly(phrase, 'textContent', randomElement.text);
+  smoothly(image, 'textContent', randomElement.image);
+});
+for(let i=0;i<2;i=i+1){
+  smoothly(phrase, 'textContent', phrases[i].text);
+   smoothly(image, 'src', phrases[i].image);}
+ 
+ WhiteMode.addEventListener('click', function () {
+  
+  this.innerHTML =
+      (this.innerHTML === 'Тёмная тема') ? this.innerHTML = 'Светлая тема' : this.innerHTML = 'Тёмная тема'
+      if(this.innerHTML === 'Светлая тема'){
+    document.body.style.color = "black";
+    document.body.style.background = "white";
+  }
+    if(this.innerHTML === 'Тёмная тема'){
+    document.body.style.color = "white";
+    document.body.style.background = "#323131";}
+ 
+ });
+
+
