8a94322253e840b9556c93c5ef46e5968c97e3b3
FalseHuman <emilkhazioff@yandex.com>
2021-06-12 18:51:04 +0500

    slider

--- cards/app.js
+++ cards/app.js
@@ -1,15 +1,25 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin() {
+    const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides){
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
+    function getRandomElement(arr) {
+        let randIndex = Math.floor(Math.random() * arr.length);
+        return arr[randIndex];
+    }
 
-        slide.classList.add('active')
-    })
+   getRandomElement(slides).classList.add('active')
+
+    for (const slide of slides){
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses(){
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
-
-function clearActiveClasses(){
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin()
