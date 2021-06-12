67f54be07286efbcf067651831f16fca4cba2c52
Emil <emilkhazioff@yandex.com>
2021-06-12 11:36:32 +0500

    smoothly-scroll

--- cards/app.js
+++ cards/app.js
@@ -6,19 +6,19 @@ function slidesPlugin() {
         return arr[randIndex];
     }
     
-   getRandomElement(slides).classList.add('active')
+   smoothly(getRandomElement(slides).classList.add('active'))
     
     for (const slide of slides){
         slide.addEventListener('click', () => {
             clearActiveClasses()
     
-            slide.classList.add('active')
+            smoothly(slide.classList.add('active'))
         })
     }
     
     function clearActiveClasses(){
         slides.forEach((slide) => {
-            slide.classList.remove('active')
+            smoothly(slide.classList.remove('active'))
         })
     }
 }
